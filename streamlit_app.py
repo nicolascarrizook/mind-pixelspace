91b8e1881d08cae6c8aac8bb0e7b37744ee89968
nicolascarrizook <68661020+nicolascarrizook@users.noreply.github.com>
2024-04-01 19:57:27 +0000

    update branch

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,38 @@
-import altair as alt
-import numpy as np
-import pandas as pd
 import streamlit as st
+import requests
 
-"""
-# Welcome to Streamlit!
+API_URL = "http://127.0.0.1:5000/generate_meditation"
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+st.title("mind.")
+st.caption("Generate personalized meditation sessions")
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+prompt = st.text_area("Enter your intention for the meditation session:", height=150)
 
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
+immersion_options = ["relaxation", "focus", "sleep"]
+immersion = st.selectbox("Select the type of immersion for your session:", immersion_options)
 
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
+voice_options = ['alloy', 'echo', 'fable', 'onyx', 'nova', 'shimmer']
+voice = st.radio("Select the voice for your session:", voice_options)
 
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
+session_durations = [5, 10, 15, 20]  # Duraciones disponibles en minutos
+session_duration = st.radio("Select the session duration (minutes):", session_durations)
 
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
 
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
+if st.button("Generate Meditation Session"):
+    with st.spinner('Wait while your meditation session is being generated...'):
+        response = requests.post(API_URL, json={"prompt": prompt, "immersion": immersion, "voice": voice, "duration": session_duration})
+        
+        if response.status_code == 200:
+            data = response.json()
+            audio_path = data.get("audio_file_path")
+            cover_image_path = data.get("cover_image_path")  # Obtener la ruta de la imagen de portada de la respuesta
+            
+            st.success("Meditation session successfully generated.")
+            st.audio(audio_path)
+            
+            if cover_image_path:
+                # Si es una ruta de archivo local, necesitas asegurarte de que Streamlit pueda acceder a ella
+                # o, si estás trabajando localmente, podría ser necesario convertir la ruta a una URL o mover el archivo a un directorio accesible
+                st.image(cover_image_path, caption="Meditation Session Cover")
+        else:
+            st.error("Error generating the meditation session. Please try again.")
