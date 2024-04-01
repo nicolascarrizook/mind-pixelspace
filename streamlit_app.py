45b8ac5aa1a4ef922e8f515a68d1712aaac2bee0
nicolascarrizook <68661020+nicolascarrizook@users.noreply.github.com>
2024-04-01 20:53:14 +0000

    update branch

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,22 +1,21 @@
 import streamlit as st
 import requests
 
-API_URL = "http://127.0.0.1:5000/generate_meditation"
-
 st.title("mind.")
 st.caption("Generate personalized meditation sessions")
 
+API_URL = st.text_input("Enter the API URL:", value="http://127.0.0.1:5000/generate_meditation")
+
 prompt = st.text_area("Enter your intention for the meditation session:", height=150)
 
-immersion_options = ["relaxation", "focus", "sleep"]
-immersion = st.selectbox("Select the type of immersion for your session:", immersion_options)
+# Reemplazar esto con un text_area
+immersion = st.text_area("Enter the type of immersion for your session:", height=50)
 
 voice_options = ['alloy', 'echo', 'fable', 'onyx', 'nova', 'shimmer']
 voice = st.radio("Select the voice for your session:", voice_options)
 
-session_durations = [5, 10, 15, 20]  # Duraciones disponibles en minutos
+session_durations = [5, 10, 15, 20] 
 session_duration = st.radio("Select the session duration (minutes):", session_durations)
-
 
 if st.button("Generate Meditation Session"):
     with st.spinner('Wait while your meditation session is being generated...'):
@@ -25,14 +24,12 @@ if st.button("Generate Meditation Session"):
         if response.status_code == 200:
             data = response.json()
             audio_path = data.get("audio_file_path")
-            cover_image_path = data.get("cover_image_path")  # Obtener la ruta de la imagen de portada de la respuesta
+            cover_image_path = data.get("cover_image_path")
             
             st.success("Meditation session successfully generated.")
             st.audio(audio_path)
             
             if cover_image_path:
-                # Si es una ruta de archivo local, necesitas asegurarte de que Streamlit pueda acceder a ella
-                # o, si estás trabajando localmente, podría ser necesario convertir la ruta a una URL o mover el archivo a un directorio accesible
                 st.image(cover_image_path, caption="Meditation Session Cover")
         else:
             st.error("Error generating the meditation session. Please try again.")
